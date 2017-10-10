085af72be35fffe0b16561fa55e47c802eb85c36
Your Name <you@example.com>
2017-10-10 13:17:04 +0800

    fix ansible api bug

--- OpsManage/utils/ansible_api_v2.py
+++ OpsManage/utils/ansible_api_v2.py
@@ -310,23 +310,27 @@ class PlayBookResultsCollector(CallbackBase):
     CALLBACK_VERSION = 2.0    
     def __init__(self, *args, **kwargs):  
         super(PlayBookResultsCollector, self).__init__(*args, **kwargs)  
-        self.task_ok = {}  
-        self.task_skipped = {}  
-        self.task_failed = {}  
-        self.task_status = {} 
+        self.task_ok = {}
+        self.task_changed = {}
+        self.task_skipped = {}
+        self.task_failed = {}
+        self.task_status = {}
         self.task_unreachable = {}
 
     def v2_runner_on_ok(self, result, *args, **kwargs):
-        self.task_ok[result._host.get_name()]  = result
+        self.task_ok[result._host.get_name()]  = result._result
+
+    def v2_runner_on_changed(self, result):
+        self.task_changed[result._host.get_name()] = result._result
 
     def v2_runner_on_failed(self, result, *args, **kwargs):
-        self.task_failed[result._host.get_name()] = result
+        self.task_failed[result._host.get_name()] = result._result
 
     def v2_runner_on_unreachable(self, result):
-        self.task_unreachable[result._host.get_name()] = result
+        self.task_unreachable[result._host.get_name()] = result._result
 
     def v2_runner_on_skipped(self, result):
-        self.task_ok[result._host.get_name()]  = result
+        self.task_ok[result._host.get_name()]  = result._result
 
     def v2_playbook_on_stats(self, stats):
         hosts = sorted(stats.processed.keys())
