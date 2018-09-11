e34418cf942e0914f829d09ce1a9e3144143150e
Welliam.Cao <303350019@qq.com>
2018-09-11 22:30:06 +0800

    修复提示import wssh不存在的bug +1

--- OpsManage/routing.py
+++ OpsManage/routing.py
@@ -1,12 +1,12 @@
 from channels.routing import route
-from OpsManage.djchannels import wssh,notices,chats
+from OpsManage.djchannels import notices,chats
 
 # The channel routing defines what channels get handled by what consumers,
 # including optional matching on message attributes. In this example, we route
 # all WebSocket connections to the class-based BindingConsumer (the consumer
 # class itself specifies what channels it wants to consume)
 channel_routing = [
-    wssh.webterminal.as_route(path = r'^/ws/webssh/(?P<id>[0-9]+)/$'),
+#     wssh.webterminal.as_route(path = r'^/ws/webssh/(?P<id>[0-9]+)/$'),
     notices.WebNotice.as_route(path = r'^/ws/notice/(?P<username>.+)/$'),
 #     chats.WebChat.as_route(path = r'^/ws/chats/$'),
 ]
