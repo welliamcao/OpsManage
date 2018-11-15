23f557a40db38efea8b8d302b3bb869d7ebcf6ba
cjzero <1362602923@qq.com>
2018-11-15 22:56:12 +0800

    添加游戏服管理

--- OpsManage/views/gameserver.py
+++ OpsManage/views/gameserver.py
@@ -22,34 +22,48 @@ from dao.assets import AssetsSource
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required()
-@permission_required('OpsManage.can_change_gameserver_config',login_url='/noperm/')
-def gameserver_list(request):
+@permission_required('OpsManage.can_read_gameserver_config',login_url='/noperm/')
+def gamehost_list(request):
     gshost = []
-    gameserverconfig = GameServer_Config.objects.select_related().all()[0:300]
-    ips = GameServer_Config.objects.values_list("ip",flat=True).distinct()
+    ips = GameServer_Config.objects.values_list("ip__ip",flat=True)
+    totalgame = GameServer_Config.objects.all().count()
+    onlinegame = 0
+    offlinegame = GameServer_Config.objects.filter(state=False).count()
     for ip in ips:
-        aid = Assets.objects.get(ip=ip).id
         sid = Server_Assets.objects.get(ip=ip).id
-        business = Assets.objects.get(ip=ip).business
+        aid = Server_Assets.objects.get(ip=ip).assets.id
+        business = Project_Assets.objects.get(id=Assets.objects.get(id=aid).business).project_name
         hostname = Server_Assets.objects.get(ip=ip).hostname
         system = Server_Assets.objects.get(ip=ip).system
-        onlinegame = GameServer_Config.objects.filter(state=True,ip=ip).count()
-        status = Assets.objects.get(ip=ip).status
-        game = GameServer_Config.objects.filter(ip=ip).values_list("name",flat=True)
+        gonlinegame = GameServer_Config.objects.filter(state=True,ip__ip=ip).count()
+        status = Assets.objects.get(id=aid).status
+        game = list(GameServer_Config.objects.filter(ip__ip=ip).values_list("name",flat=True))
+        onlinegame = onlinegame+gonlinegame
         gshost.append(
             {
-                "aid":aid,
-                "sid":sid,
-                "business":business,
-                "hostname":hostname,
-                "ip":ip,
-                "system":system,
-                "onlinegame":onlinegame,
-                "game":game,
-                "status":status,
+                'game': game,
+                'aid':aid,
+                'sid':sid,
+                'business':business,
+                'hostname':hostname,
+                'ip':ip,
+                'system':system,
+                'onlinegame':gonlinegame,
+                'status':status,
             }
         )
-    return render(request,'gameserver/gs_config.html',{"user":request.user,"gshost":gshost})
-def gamehost_facts(){
+    return render(request,'gameserver/gs_config.html',{"user":request.user,"totalGame":totalgame,"onlineGame":onlinegame,"offlineGame":offlinegame,"gshost":gshost},)
 
-}
+@login_required()
+@permission_required('OpsManage.can_read_gameserver_config')
+def gameserver_details(request):
+    gameserver_list = GameServer_Config.objects.all()[0:300]
+    if request.method == "GET" :
+        #rungame = ANSRunner.run_model()
+        return render(request.user,{"gs_list":gameserver_list})
+    if request.method == "POST" and request.user.has_perm('OpsManage.can_change_gameserver_config'):
+        pass
+    if request.method == "DELETE" and request.user.has_perm('OpsManage.can_delete_gameserver_config'):
+        pass
+
+
