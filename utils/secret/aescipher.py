6e635959412333c2e300baa246b61a14acd48f1f
Welliam.Cao <303350019@qq.com>
2020-04-25 15:55:31 +0800

    .

--- utils/secret/aescipher.py
+++ utils/secret/aescipher.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python  
 # _#_ coding:utf-8 _*_
+# 参考https://pypi.org/project/obscure-api/
+
 import base64
 import hashlib
 
@@ -29,32 +31,3 @@ class AESCipher(object):
     @staticmethod
     def _unpad(s):
         return s[:-ord(s[len(s)-1:])]
-
-
-# BS = 16
-# 
-# 
-# def pad(s):
-#     return s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-# 
-# 
-# def unpad(s):
-#     return s[0:-s[-1]]
-# 
-# 
-# class AESCipher:
-# 
-#     def __init__(self, key):
-#         self.key = hashlib.sha256(key.encode('utf-8')).digest()
-# 
-#     def encrypt(self, raw):
-#         raw = pad(raw)
-#         iv = Random.new().read(AES.block_size)
-#         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-#         return base64.b64encode(iv + cipher.encrypt(raw))
-# 
-#     def decrypt(self, enc):
-#         enc = base64.b64decode(enc)
-#         iv = enc[:16]
-#         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-#         return unpad(cipher.decrypt(enc[16:]))
